ba980ca7285abb334d2464bec77de7f26c705007
Maksim Bogatov <maxbog@inbox.ru>
2019-06-27 23:44:19 +0300

    4

---
+++ MailSender/MailSender.lib/MVVM/LamdaCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MailSender.lib.MVVM
+{
+    public class LamdaCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private readonly Action<object> _CommandAction;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LamdaCommand(Action<object> CommandAction, Func<object, bool> CanExecute = null)
+        {
+            _CommandAction = CommandAction ?? throw new ArgumentNullException(nameof(CommandAction));
+            _CanExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _CommandAction(parameter);
+    }
+}
---
+++ MailSender/MailSender/Commands/ApplicationCloseCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MailSender.lib.MVVM;
+
+namespace MailSender.Commands
+{
+    class ApplicationCloseCommand : LamdaCommand
+    {
+        public ApplicationCloseCommand() : base(p => Application.Current.Shutdown()) { }
+    }
+}
